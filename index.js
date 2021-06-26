f11184ce4567390808e304d581d79f3e6ab4082d
twinh <twinhuang@qq.com>
2021-06-26 23:15:46 +0800

    feat(m-footer-bar): `FooterBar` 加载完成后，自动更新页面底部边距，以便页面显示完整
    - h5 页面会隐藏起来，直接查询 .mx-footer-bar 会获取到上个页面的页脚，高度变 0，改为通过唯一 id 获取

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
+import {useEffect, useContext, useState} from 'react';
 import {View} from '@fower/taro';
 import {default as BaseButton} from '@mxjs/m-button';
 import clsx from 'clsx';
 import './index.scss';
+import Taro from '@tarojs/taro';
+import {PageContext} from '@mxjs/m-page';
 import PropTypes from 'prop-types';
 
-const FooterBar = ({className, children, ...props}) => {
+let counter = 0;
+
+const FooterBar = ({className, children, id: idProp, ...props}) => {
+  const {setCss} = useContext(PageContext);
+  const [id] = useState(() => {
+    if (typeof idProp !== 'undefined') {
+      return idProp;
+    } else {
+      return '__mx-footer-bar-' + ++counter;
+    }
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      const query = Taro.createSelectorQuery();
+      query.select('#' + id).boundingClientRect(rect => {
+        if (!rect) {
+          return;
+        }
+
+        setCss({pb: rect.height + 'px'});
+      }).exec();
+    });
+  }, []);
+
   return (
-    <View className={clsx('mx-footer-bar', className)} {...props}>{children}</View>
+    <View id={id} className={clsx('mx-footer-bar', className)} {...props}>{children}</View>
   );
 };
 
